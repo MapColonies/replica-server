578125f5b4130e25949e84ba5c2b2312446287ab
razbroc <razbroc@gmail.com>
2025-02-10 14:13:32 +0200

    fix: imtegration app

--- tests/integration/layer/helpers/requestSender.ts
+++ tests/integration/layer/helpers/requestSender.ts
@@ -1,7 +1,8 @@
+import { Application } from 'express';
 import * as supertest from 'supertest';
 
 export class LayerRequestSender {
-  public constructor(private readonly app: Express.Application) {}
+  public constructor(private readonly app: Application) {}
 
   public async getLayers(): Promise<supertest.Response> {
     return supertest.agent(this.app).get('/layers');
