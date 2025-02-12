76d9d2dccd32dc1d146ae3e61ff6a4e683bd5e1f
razbroc <razbroc@gmail.com>
2025-02-12 15:55:52 +0200

    fix: dataSource.ts

--- dataSource.ts
+++ dataSource.ts
@@ -1,26 +1,21 @@
-// import { DataSource } from 'typeorm';
-// import { getConfig, initConfig } from './src/common/config';
-// import { convertDBConfigToTypeorm } from './src/common/utils/configModifier';
-// import { createConnectionOptions } from './src/common/db';
+import { DataSource } from 'typeorm';
+import { getConfig, initConfig } from './src/common/config';
+import { convertDBConfigToTypeorm } from './src/common/utils/configModifier';
+import { createConnectionOptions } from './src/common/db';
 
 
-// async function getAppDataSource(): Promise<DataSource>{
-//   await initConfig(true);
-//   const config = getConfig();
-//   const dataSourceOptions = config.get('db');
+async function getAppDataSource(): Promise<DataSource>{
+  await initConfig(true);
+  const config = getConfig();
+  const dataSourceOptions = config.get('db');
 
-//   return new DataSource({
-//     ...createConnectionOptions(convertDBConfigToTypeorm(dataSourceOptions)),
-//     entities: ['src/**/DAL/typeorm/*.ts'],
-//     migrationsTableName: 'migrations_table',
-//     migrations: ['db/migrations/*.ts'],
-//   });
+  return new DataSource({
+    ...createConnectionOptions(convertDBConfigToTypeorm(dataSourceOptions)),
+    entities: ['src/**/DAL/typeorm/*.ts'],
+    migrationsTableName: 'migrations_table',
+    migrations: ['db/migrations/*.ts'],
+  });
   
-// }
+}
 
-// export const appDataSource = new DataSource({
-//   ...createConnectionOptions(convertDBConfigToTypeorm(dataSourceOptions)),
-//   entities: ['src/**/DAL/typeorm/*.ts'],
-//   migrationsTableName: 'migrations_table',
-//   migrations: ['db/migrations/*.ts'],
-// });
+export const appDataSource : Promise<DataSource> = getAppDataSource();
