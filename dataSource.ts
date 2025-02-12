bd052c92c4257c9838aaffd9b95b8805f70eaf9c
razbroc <razbroc@gmail.com>
2025-02-12 16:05:24 +0200

    fix: dataSource.ts

--- dataSource.ts
+++ dataSource.ts
@@ -1,21 +1,38 @@
-import { DataSource } from 'typeorm';
+import { DataSource, Entity } from 'typeorm';
 import { getConfig, initConfig } from './src/common/config';
 import { convertDBConfigToTypeorm } from './src/common/utils/configModifier';
 import { createConnectionOptions } from './src/common/db';
 
+// async function getAppDataSource(): Promise<DataSource>{
+//   await initConfig(true);
+//   const config = getConfig();
+//   const dataSourceOptions = config.get('db');
 
-async function getAppDataSource(): Promise<DataSource>{
+//   return new DataSource({
+//     ...createConnectionOptions(convertDBConfigToTypeorm(dataSourceOptions)),
+//     entities: ['src/**/DAL/typeorm/*.ts'],
+//     migrationsTableName: 'migrations_table',
+//     migrations: ['db/migrations/*.ts'],
+//   });
+  
+// }
+
+//export const appDataSource : Promise<DataSource> = getAppDataSource();
+
+const dataSourceFactory = async (): Promise<DataSource> => {
   await initConfig(true);
   const config = getConfig();
-  const dataSourceOptions = config.get('db');
 
-  return new DataSource({
-    ...createConnectionOptions(convertDBConfigToTypeorm(dataSourceOptions)),
-    entities: ['src/**/DAL/typeorm/*.ts'],
-    migrationsTableName: 'migrations_table',
-    migrations: ['db/migrations/*.ts'],
+  const connectionOptions = convertDBConfigToTypeorm(config.get('db'));
+
+  const appDataSource = new DataSource({
+    ...createConnectionOptions(connectionOptions),
+    entities: [Entity, 'src/entity/models/*.ts'],
+    migrationsTableName: 'custom_migration_table',
+    migrations: ['db/migration/*.ts'],
   });
-  
-}
 
-export const appDataSource : Promise<DataSource> = getAppDataSource();
+  return appDataSource;
+};
+
+export default dataSourceFactory();
