cc12683d5114fdca6e0a8638bb63f213a1dc2e5b
Niv Greenstein <greenstein9@gmail.com>
2025-03-05 12:24:22 +0200

    feat: added ON_SIGNAL, HEALTHCHECK

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,9 +1,11 @@
+import { readPackageJsonSync } from '@map-colonies/read-pkg';
+
 export const DEFAULT_SERVER_PORT = 80;
 
 export const IGNORED_OUTGOING_TRACE_ROUTES = [/^.*\/v1\/metrics.*$/];
 export const IGNORED_INCOMING_TRACE_ROUTES = [/^.*\/docs.*$/, /^.*\/metrics.*/];
 
-export const SERVICE_NAME = 'replica-server';
+export const SERVICE_NAME = readPackageJsonSync().name ?? 'unknown_service';
 
 /* eslint-disable @typescript-eslint/naming-convention */
 export const SERVICES = {
@@ -12,9 +14,13 @@ export const SERVICES = {
   TRACER: Symbol('Tracer'),
   METRICS: Symbol('Metrics'),
   OBJECT_STORAGE: Symbol('IObjectStorage'),
+  CLEANUP_REGISTRY: Symbol('CleanupRegistry'),
 } satisfies Record<string, symbol>;
 /* eslint-enable @typescript-eslint/naming-convention */
 
 export const BUCKET_NAME_MIN_LENGTH_LIMIT = 3;
 
 export const BUCKET_NAME_MAX_LENGTH_LIMIT = 63;
+
+export const HEALTHCHECK = Symbol('healthcheck');
+export const ON_SIGNAL = Symbol('onSignal');
